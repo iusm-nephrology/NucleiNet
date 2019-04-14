b9cde07d9efef4328ed388bdee59dce260a405f2
Andre Woloshuk <awoloshuk@gmail.com>
2019-04-14 18:43:02 -0400

    Add files via upload

---
+++ Andre/MNIST_Example/model/metric.py
@@ -0,0 +1,20 @@
+import torch
+#Define new metrics for training here
+
+def my_metric(output, target): #accuracy
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
+    return correct / len(target)
+
+
+def my_metric2(output, target, k=3): #top 3 accuracy
+    with torch.no_grad():
+        pred = torch.topk(output, k, dim=1)[1]
+        assert pred.shape[0] == len(target)
+        correct = 0
+        for i in range(k):
+            correct += torch.sum(pred[:, i] == target).item()
+    return correct / len(target)
