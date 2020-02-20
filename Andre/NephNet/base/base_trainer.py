84eacfdd7a97581813a5ba75b8b870cf994e86ea
Andre Woloshuk <awoloshuk@gmail.com>
2020-02-20 15:01:30 -0500

    Add files via upload

--- Andre/NephNet/base/base_trainer.py
+++ Andre/NephNet/base/base_trainer.py
@@ -85,6 +85,7 @@ class BaseTrainer:
         """
         Full training logic
         """
+        not_improved_count = 0
         for epoch in range(self.start_epoch, self.epochs + 1):
             result = self._train_epoch(epoch)
 
